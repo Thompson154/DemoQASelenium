6469cad6e501369d9f32a151b913578ccc1233a6
Adrian Thompson <thompsonadrian210@gmail.com>
2024-11-28 10:23:02 -0400

    Initial Project

---
+++ src/test/java/controls/Button.java
@@ -0,0 +1,10 @@
+package controls;
+
+import org.openqa.selenium.By;
+
+public class Button extends Controls {
+
+    public Button(By locator){
+        super(locator);
+    }
+}
---
+++ src/test/java/controls/Controls.java
@@ -0,0 +1,44 @@
+package controls;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import session.Session;
+
+public class Controls {
+
+    protected By locator;
+
+    protected WebElement control;
+
+    public Controls(By locator) {
+        this.locator = locator;
+    }
+
+    protected void findControl() {
+        this.control = Session.getInstance().getBrowser().findElement(locator);
+    }
+
+    public void click() {
+        this.findControl();
+        this.control.click();
+    }
+
+    public boolean isControlDisplayed() {
+        try {
+            this.findControl();
+            return this.control.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getText(){
+        this.findControl();
+        return control.getText();
+    }
+
+    public String getValue(){
+        this.findControl();
+        return control.getAttribute("value");
+    }
+}
